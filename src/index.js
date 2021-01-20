fe3f65da09a00e6c57901192bbfc7e70cd494280
leo1109 <windpass71@yahoo.co.jp>
2021-01-20 13:29:05 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div class="header">
+                <h1>React Sample Page</h1>
+                <span>This is a description message.</span>
+            </div>
+        )
+    }
+}
+
+class TimerButton extends React.Component {
+    render() {
+        return (
+            <button className="timerButton">
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+class Content extends React.Component {
+    renderButton(i) {
+        return <TimerButton value={i} />;
+    }
+    render() {
+        return (
+            <div className="content">
+                <div className="content-description">
+                    <span className="content-description-text">This is a content article.</span>
+                </div>
+                <div className="content-description-buttons">
+                    {this.renderButton(15)}
+                    {this.renderButton(60)}
+                    {this.renderButton(180)}
+                    {this.renderButton('ext')}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div class="footer">
+                <p>Copy right message</p>
+            </div>
+        )
+    }
+}
+
+class Page extends React.Component {
+    render() {
+        return (
+            <div className="page">
+                <div className="header">
+                    <Header />
+                </div>
+                <div className="content">
+                    <Content />
+                </div>
+                <div className="footer">
+                    <Footer />
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Page />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
